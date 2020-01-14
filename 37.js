c20474fc3121696b760809ba25a50d40327043d7
ankit0537 <59608445+ankit0537@users.noreply.github.com>
2020-01-14 11:08:42 +0530

    Add files via upload

---
+++ 37.js
@@ -0,0 +1,15 @@
+    function div(num){
+        if (num%3==0){
+
+            
+            
+        return true
+        }
+        if(num%7==0){
+            
+            return true
+            }
+        
+        return false
+        }
+        console.log(div(27));
